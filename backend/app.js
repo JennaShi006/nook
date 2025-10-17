9baeeb9bc5ea0075d24c3283a3ea1ee3569bb495
Jenna Shi <jennashi0927@gmail.com>
2025-10-16 22:45:15 -0400

    added package-lock.json

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,6 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 // Example route
 app.get("/", (req, res) => {
   res.send("API is running...");
